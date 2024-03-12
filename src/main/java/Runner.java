0ed407a7b91416871925cd34988867ce68d087a1
Akshit Sinha <akshitsinha3@gmail.com>
2024-03-12 14:12:18 +0530

    format

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -26,48 +26,48 @@ public class Runner {
     );
 
     private AircraftFactory passengerPlaneFactory = new PassengerPlaneFactory();
-private AircraftFactory helicopterFactory = new HelicopterFactory();
-private AircraftFactory passengerDroneFactory = new PassengerDroneFactory();
+    private AircraftFactory helicopterFactory = new HelicopterFactory();
+    private AircraftFactory passengerDroneFactory = new PassengerDroneFactory();
 
 
-Aircraft a = passengerPlaneFactory.createAircraft("A380");
-Aircraft b = passengerPlaneFactory.createAircraft("A350");
-Aircraft c = passengerPlaneFactory.createAircraft("Embraer 190");
-Aircraft d = passengerPlaneFactory.createAircraft("Antonov AN2");
-Aircraft e = helicopterFactory.createAircraft("H1");
-Aircraft f = passengerDroneFactory.createAircraft("HypaHype");
+    Aircraft a = passengerPlaneFactory.createAircraft("A380");
+    Aircraft b = passengerPlaneFactory.createAircraft("A350");
+    Aircraft c = passengerPlaneFactory.createAircraft("Embraer 190");
+    Aircraft d = passengerPlaneFactory.createAircraft("Antonov AN2");
+    Aircraft e = helicopterFactory.createAircraft("H1");
+    Aircraft f = passengerDroneFactory.createAircraft("HypaHype");
 
-List<Aircraft> aircrafts = Arrays.asList(a, b, c, d, e, f);
-
-
+    List<Aircraft> aircrafts = Arrays.asList(a, b, c, d, e, f);
 
 
-//     static List<Object> aircrafts = Arrays.asList(
-//             new PassengerPlane("A380"),
-//             new PassengerPlane("A350"),
-//             new PassengerPlane("Embraer 190"),
-//             new PassengerPlane("Antonov AN2"),
-//             new Helicopter("H1"),
-//             new PassengerDrone("HypaHype")
-//     );
 
-List<Flight> flights = Arrays.asList(
-            new Flight(1, airports.get(0), airports.get(1), aircrafts.get(0)),
-            new Flight(2, airports.get(1), airports.get(2), aircrafts.get(1)),
-            new Flight(3, airports.get(2), airports.get(4), aircrafts.get(2)),
-            new Flight(4, airports.get(3), airports.get(2), aircrafts.get(3)),
-            new Flight(5, airports.get(4), airports.get(2), aircrafts.get(4)),
-            new Flight(6, airports.get(5), airports.get(7), aircrafts.get(5))
-    );
 
-//     static List<Flight> flights = Arrays.asList(
-//             new Flight(1, airports.get(0), airports.get(1), aircrafts.get(0)),
-//             new Flight(2, airports.get(1), airports.get(2), aircrafts.get(1)),
-//             new Flight(3, airports.get(2), airports.get(4), aircrafts.get(2)),
-//             new Flight(4, airports.get(3), airports.get(2), aircrafts.get(3)),
-//             new Flight(5, airports.get(4), airports.get(2), aircrafts.get(4)),
-//             new Flight(6, airports.get(5), airports.get(7), aircrafts.get(5))
-//     );
+    //     static List<Object> aircrafts = Arrays.asList(
+    //             new PassengerPlane("A380"),
+    //             new PassengerPlane("A350"),
+    //             new PassengerPlane("Embraer 190"),
+    //             new PassengerPlane("Antonov AN2"),
+    //             new Helicopter("H1"),
+    //             new PassengerDrone("HypaHype")
+    //     );
+
+    List<Flight> flights = Arrays.asList(
+                new Flight(1, airports.get(0), airports.get(1), aircrafts.get(0)),
+                new Flight(2, airports.get(1), airports.get(2), aircrafts.get(1)),
+                new Flight(3, airports.get(2), airports.get(4), aircrafts.get(2)),
+                new Flight(4, airports.get(3), airports.get(2), aircrafts.get(3)),
+                new Flight(5, airports.get(4), airports.get(2), aircrafts.get(4)),
+                new Flight(6, airports.get(5), airports.get(7), aircrafts.get(5))
+        );
+
+    //     static List<Flight> flights = Arrays.asList(
+    //             new Flight(1, airports.get(0), airports.get(1), aircrafts.get(0)),
+    //             new Flight(2, airports.get(1), airports.get(2), aircrafts.get(1)),
+    //             new Flight(3, airports.get(2), airports.get(4), aircrafts.get(2)),
+    //             new Flight(4, airports.get(3), airports.get(2), aircrafts.get(3)),
+    //             new Flight(5, airports.get(4), airports.get(2), aircrafts.get(4)),
+    //             new Flight(6, airports.get(5), airports.get(7), aircrafts.get(5))
+    //     );
 
     static Schedule schedule;
 
