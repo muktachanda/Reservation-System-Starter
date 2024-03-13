34ae9b6d2fadcdf553a1361c81f7949e7c101069
Akshit Sinha <akshitsinha3@gmail.com>
2024-03-13 19:28:12 +0530

    indents

--- src/main/java/flight/reservation/plane_factory/PassengerPlaneFactory.java
+++ src/main/java/flight/reservation/plane_factory/PassengerPlaneFactory.java
@@ -14,56 +14,55 @@ public class PassengerPlaneFactory extends AircraftFactory {
 
     // create a380
     public static PassengerPlane createA380(PlaneDirector director) {
-
-            A380Builder builder = new A380Builder();
-            director.setPlaneType(builder);
-            director.constructPlane();
-            PassengerPlane a380 = builder.getPlane();
-            return a380;
+        A380Builder builder = new A380Builder();
+        director.setPlaneType(builder);
+        director.constructPlane();
+        PassengerPlane a380 = builder.getPlane();
+        return a380;
     }
 
     // create a350
     public static PassengerPlane createA350(PlaneDirector director) {
-            A350Builder builder = new A350Builder();
-            director.setPlaneType(builder);
-            director.constructPlane();
-            PassengerPlane a350 = builder.getPlane();
-            return a350;
+        A350Builder builder = new A350Builder();
+        director.setPlaneType(builder);
+        director.constructPlane();
+        PassengerPlane a350 = builder.getPlane();
+        return a350;
     }
 
     // create embraer 190
     public static PassengerPlane createEmbraer190(PlaneDirector director) {
-            Embraer190Builder builder = new Embraer190Builder();
-            director.setPlaneType(builder);
-            director.constructPlane();
-            PassengerPlane embraer190 = builder.getPlane();
-            return embraer190;
+        Embraer190Builder builder = new Embraer190Builder();
+        director.setPlaneType(builder);
+        director.constructPlane();
+        PassengerPlane embraer190 = builder.getPlane();
+        return embraer190;
     }
 
 
     // create antonov an2
     public static PassengerPlane createAntonovAN2(PlaneDirector director) {
-            AntonovAN2Builder builder = new AntonovAN2Builder();
-            director.setPlaneType(builder);
-            director.constructPlane();
-            PassengerPlane antonovAN2 = builder.getPlane();
-            return antonovAN2;
+        AntonovAN2Builder builder = new AntonovAN2Builder();
+        director.setPlaneType(builder);
+        director.constructPlane();
+        PassengerPlane antonovAN2 = builder.getPlane();
+        return antonovAN2;
     }
 
 
     public static PassengerPlane createPassengerPlane(String model) {
-            switch (model) {
-                    case "A380":
-                            return createA380(new PlaneDirector());
-                    case "A350":
-                            return createA350(new PlaneDirector());
-                    case "Embraer 190":
-                            return createEmbraer190(new PlaneDirector());
-                    case "Antonov AN2":        
-                            return createAntonovAN2(new PlaneDirector());
+        switch (model) {
+                case "A380":
+                        return createA380(new PlaneDirector());
+                case "A350":
+                        return createA350(new PlaneDirector());
+                case "Embraer 190":
+                        return createEmbraer190(new PlaneDirector());
+                case "Antonov AN2":        
+                        return createAntonovAN2(new PlaneDirector());
 
-                    default:
-                            return createAntonovAN2(new PlaneDirector());
-            }
+                default:
+                        return createAntonovAN2(new PlaneDirector());
+        }
     }
 }
